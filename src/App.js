497401a0b646f809cbd7406c85a34d29ebe90cf5
Hector Rodriguez <hector@hrod.dev>
2022-10-19 10:22:47 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
+  const [isHover, setIsHover] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHover(true);
+  };
+
+  const handelMouseLeave = () => {
+    setIsHover(false);
+  };
+
+  const handleDobleClic = () => {
+    setIsHover(!isHover);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (isHover === true) {
+        setRed(Math.floor(Math.random() * 255));
+        setGreen(Math.floor(Math.random() * 255));
+        setBlue(Math.floor(Math.random() * 255));
+      }
+    }, 500);
+    return () => clearInterval(interval);
+  }, [isHover]);
+
+  const boxStyle = {
+    height: "500px",
+    width: "500px",
+    display: "flex",
+    justifyContent: "center",
+    cursor: "pointer",
+    backgroundColor: `rgb(${red}, ${green}, ${blue})`,
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <title>Square random colors</title>
+        <h1>
+          Square Random Color <br />
+          Color: {boxStyle.backgroundColor}
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <strong>Hover the mouse</strong> over the square and this will change
+          it the color randomizer. <br/>If the mouse hover leave the square, the change it will stop. <br/>Press <strong>double click</strong> on the
+          square for stop/start it. 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <div
+            style={boxStyle}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handelMouseLeave}
+            onDoubleClick={handleDobleClic}
+          ></div>
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
